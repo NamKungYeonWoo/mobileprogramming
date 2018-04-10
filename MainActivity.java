bd0bcf464e558c9bdedf0ea2681d69886fa4b010
NamKungYeonWoo <33145624+NamKungYeonWoo@users.noreply.github.com>
2018-04-10 20:07:30 +0900

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,40 +1,34 @@
-package org.androidtown.hello;
-import android.content.Context;
+package com.example.namgoong.myapplication;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-//import org.androidtown.hello.changeimage.R;
-
+import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
-    ImageView imageView;
-    ImageView imageView2;                              // 이미지객체 생성
-    int imageIndex=0;
+    EditText Name;
+    EditText Age;
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView= (ImageView)findViewById(R.id.blue);     // 안드로이드 블루 이미지 참조
-        imageView2=(ImageView)findViewById(R.id.red);      // 안드로이드 레드 이미지 참조
-    }
-    public void onButton1Clicked(View v){                   // 버튼클릭시 실행할 함수
-        changeImage();			// 버튼클릭시 changeImage method 실행
-
-    }
-    private void changeImage(){
-        if(imageIndex==0){                                        // imageIndex가 0이면 블루이미지
-            imageView.setVisibility(View.VISIBLE);
-            imageView2.setVisibility(View.INVISIBLE);
-            imageIndex=1;}	// imageIndex 1로 바꿔 다음 버튼       누르면 레드이미지로 바꾸게끔 한다.
-
-        else if(imageIndex==1){ 		// imageIndex가 1이면 레드이미지
-            imageView.setVisibility(View.INVISIBLE);
-            imageView2.setVisibility(View.VISIBLE);
-            imageIndex=0;
-        }
+        Name = (EditText) findViewById(R.id.editText1);
+        Age = (EditText) findViewById(R.id.editText2);
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String UserName = Name.getText().toString();
+                String UserAge = Age.getText().toString();
+                Intent intent = new Intent (getApplicationContext(), ActivityNew.class);
+                intent.putExtra("loginName", UserName);
+                intent.putExtra("loginAge", UserAge);
+                startActivity(intent);
+            }
+        });
     }
 }
